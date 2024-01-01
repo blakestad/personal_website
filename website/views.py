b8422027e36d89b6448f43a51f394a184dd2c872
Cliff Blakestad <data.blakestad@gmail.com>
2024-01-01 14:59:30 -0600

    Introduced subject identifier service

--- website/views.py
+++ website/views.py
@@ -31,8 +31,37 @@ def recommend():
             return 'Failed to get recommendations', 500
     return render_template('recommend.html')
 
-@views.route('/projects/blog/arxiv')
-def recommend_blog():
+@views.route('/projects/subject-identifier', methods=['GET','POST'])
+def identify_subject():
+    if request.method == 'POST':
+        input_title=request.form.get('title')
+        input_abstract=request.form.get('abstract')
+
+        # Create payload
+        payload = {'input_title': input_title, 'input_abstract': input_abstract}
+
+        # The URL of Google Cloud Run service that runs the identification backend
+        identification_service_url = 'http://127.0.0.1:8080'
+        #identification_service_url = '????'
+
+        # Make the POST request
+        response = requests.post(identification_service_url, json=payload)
+
+        if response.status_code == 200:
+            results = pd.DataFrame(response.json())
+            results.sort_values('prob',ascending=False,inplace=True)
+            if results['guess'].sum() > 0:
+                positive_results = results[results['guess']]
+                return render_template('subject_identifier_output.html', title=input_title, abstract=input_abstract, results=positive_results)
+            else:
+                top_results = results.head(3)
+                return render_template('subject_identifier_invalid_output.html', title=input_title, abstract=input_abstract, results=top_results)
+        else:
+            return 'Failed to get identifications', 500
+    return render_template('subject_identifier.html')
+
+@views.route('/blog/arxiv')
+def arxiv_blog():
     return render_template('arxiv_blog.html')
 
 @views.route('/math')
